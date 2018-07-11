5164287ffbad136cb9135fd1ed2bf80f1b776040
dsantoro <davide.santoro.it@gmail.com>
2018-07-11 11:41:01 +0200

    1

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -49,65 +49,16 @@ app.intent("AMAZON.CancelIntent", {
 );
 
 app.intent('SumPlayer',
-	{
-		"slots":{"first_num":"NUMBER", "second_num":"NUMBER"}
-		,"utterances":[
-			"sum {1-100|first_num} and {1-100|second_num}",
-			"sum two numbers",
-			"the first number is {!-100|first_num}",
-			"the second number is {!-100|second_num}", "{!-100|num}"]
-	},
 	function(request,response) {
-		var session = null;
-		var num1 = null;
-		var num2 = null;
 		console.log("Request initiated");
 		// check if you can use session (read or write)
-		if(request.hasSession()){
-			session = request.getSession();
-			num1 = session.get('first_num');
-			num2 = session.get('second_num');
-			console.log("Getting session with first num: "+num1+" and second num: "+num2+"!");
-		}
 		// get the session object
 		var arg1 = request.slot('first_num');
 		var arg2 = request.slot('second_num');
-		var num = request.slot('num');
-		console.log("Getting slots from request with first num: "+arg1+", second num: "+arg2+" and num: "+num);
-		if(num1==null){
-			console.log("Session: first_num not present");
-			if(arg1 == null && num == null){
-				console.log("Request: first_num and num not present");
-				var say = "You need to give the first number";
-				var prompt = "What's the first number?";
-				response.say(say).reprompt(prompt).shouldEndSession(false);
-			}else if (arg1 != null){
-				console.log("Request: first_num present");
-				session.set("first_num",arg1);
-			}else if (num != null){
-				console.log("Request: num present");
-				session.set("first_num",num);
-			}
-		}else if(num2==null){
-			console.log("Session: second_num not present");
-			if(arg2 == null && num == null){
-				console.log("Session: second_num and num not present");
-				var say = "You need to give the second number";
-				var prompt = "What's the second number?";
-				response.say(say).reprompt(prompt).shouldEndSession(false);
-			}else if (arg2 != null){
-				console.log("Request: second_num present");
-				session.set("second_num",arg2);
-			}else if (num != null){
-				console.log("Request: num present");
-				session.set("second_num",num);
-			}
-		} else {
-			console.log("Session: first_num: "+num1+" and second_num: "+num2+" present");
-			var res = parseInt(num1) + parseInt(num2);
-			res  = "The sum between "+num1+" and "+num2+" is equeal to "+res.toString();
-			response.say(res);
-		}
+		console.log("Getting slots from request with first num: "+arg1+", second num: "+arg2);
+		var res = parseInt(arg1) + parseInt(arg2);
+		res  = "The sum between "+arg1+" and "+arg2+" is equeal to "+res.toString();
+		response.say(res);
 	}
 );
 
